ff36fdefdc78d5ad9ecb0f91e114669246347c26
Greyson Harris <gharris@esri.com>
2019-01-17 00:17:36 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
-window.addEventListener("load", () => {
+window.addEventListener("load", function (e) {
   //start your code here
-});
+ 
+  var passwordLength
+  
+  document.getElementById("passwordSubmit").addEventListener("click", function (e) {
+    event.preventDefault();
+    passwordLength = document.getElementById("passwordLength").value;
+    
+    // calculatePassword(passwordLength);
+    document.getElementById("passwordResult").innerHTML = calculatePassword(passwordLength);
+  });
+
+  //This calculates a random password
+  function calculatePassword(length) {
+    
+    //These four functions calculate each of the required characters
+    function getUppercaseLetter() {
+      return String.fromCharCode(65 + Math.floor(Math.random() * 26));
+    }
+    function getLowercaseLetter() {
+      return  getUppercaseLetter().toLowerCase();
+    }
+    function getSpecialChar() {
+      return String.fromCharCode(33 + Math.floor(Math.random() * 15));
+    }
+    function getNumber() {
+      return Math.floor(Math.random() * 10);
+    }
+    
+    //This concatenates the four required characters into a variable, and loops through lower case letters to fill out required password length
+    
+    var password = getUppercaseLetter() + getLowercaseLetter() + getSpecialChar() + getNumber();
+    
+    for (var i = passwordLength - 4; i > 0; i--) {
+    password = password + getLowercaseLetter();
+    }
+    console.log(password);
+    return password;
+  }
+  
+}); 
